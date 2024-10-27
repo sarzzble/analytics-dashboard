b4b437dcf64ae084a36bf6c30e116bcf75da1763
Salih Demir <salihdemir436@gmail.com>
2024-10-27 11:36:25 +0300

    initial commit

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { analytics } from "./utils/analytics";
+
+export default async function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname === "/") {
+    // track analytics event
+
+    try {
+      analytics.track("pageview", {
+        page: "/",
+        country: req.geo?.country,
+      });
+    } catch (error) {
+      // fail silently
+      console.log(error);
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const matcher = {
+  matcher: ["/"],
+};
